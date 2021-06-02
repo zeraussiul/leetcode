bdfacb3003239a3bd84f6212da25a4656854d0b5
Luis Suarez <luis.suarez88@gmail.com>
2021-06-02 10:47:16 -0400

    Problem 200: Number of Islands

---
+++ p0200_numIslands/islands.go
@@ -0,0 +1,33 @@
+package islands
+
+func numIslands(grid [][]byte) int {
+	// create and size visited grid to keep track of visited nodes
+	visited := make([][]bool, len(grid))
+	for i := 0; i < len(visited); i++ {
+		visited[i] = make([]bool, len(grid[0]))
+	}
+	islands := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if !visited[i][j] && grid[i][j] == '1' {
+				dfs(i, j, grid, visited)
+				islands++
+			}
+		}
+	}
+	return islands
+}
+
+func dfs(i, j int, grid [][]byte, visited [][]bool) {
+	// base case here
+	// if out of bounds, return
+	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || visited[i][j] || grid[i][j] == '0' {
+		return
+	}
+	// rest
+	visited[i][j] = true
+	dfs(i+1, j, grid, visited)
+	dfs(i-1, j, grid, visited)
+	dfs(i, j+1, grid, visited)
+	dfs(i, j-1, grid, visited)
+}
