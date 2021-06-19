b2602aa306ba8b31c6fb8a033a3895b5d2fa0f3d
Luis Suarez <luis.suarez88@gmail.com>
2021-06-19 11:10:32 -0400

    Problem: Max Depth of Binary Tree

---
+++ p0104_maxDepth/max.go
@@ -0,0 +1,28 @@
+package max
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	if left >= right {
+		return left + 1
+	} else {
+		return right + 1
+	}
+}
